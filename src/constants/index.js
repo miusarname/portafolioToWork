7275e78f388fbdd8fa1dde5dc591124ffcb0e225
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2023-07-29 01:22:49 -0500

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,87 @@
+export const projects = [
+  {
+    title: "On work..",
+    desc: "On work..",
+    tags: [
+      {
+        name: "NextTs",
+        color: "red-600",
+      },
+      {
+        name: "TailwindCSS",
+        color: "green-600",
+      },
+    ],
+    img: "https://static2.larioja.com/www/multimedia/202203/13/media/VenderCoche_DaniMaiz.gif",
+    link: "https://",
+    code: "",
+  },
+  {
+    title: "On work..",
+    desc: "On work..",
+    tags: [
+      {
+        name: "NextTs",
+        color: "red-600",
+      },
+      {
+        name: "TailwindCSS",
+        color: "green-600",
+      },
+    ],
+    img: "https://static2.larioja.com/www/multimedia/202203/13/media/VenderCoche_DaniMaiz.gif",
+    link: "https://",
+    code: "",
+  },
+  {
+    title: "On work..",
+    desc: "On work..",
+    tags: [
+      {
+        name: "NextTs",
+        color: "red-600",
+      },
+      {
+        name: "TailwindCSS",
+        color: "green-600",
+      },
+    ],
+    img: "https://static2.larioja.com/www/multimedia/202203/13/media/VenderCoche_DaniMaiz.gif",
+    link: "https://",
+    code: "",
+  },
+  {
+    title: "On work..",
+    desc: "On work..",
+    tags: [
+      {
+        name: "NextTs",
+        color: "red-600",
+      },
+      {
+        name: "TailwindCSS",
+        color: "green-600",
+      },
+    ],
+    img: "https://static2.larioja.com/www/multimedia/202203/13/media/VenderCoche_DaniMaiz.gif",
+    link: "https://",
+    code: "",
+  },
+  {
+    title: "On work..",
+    desc: "On work..",
+    tags: [
+      {
+        name: "NextTs",
+        color: "red-600",
+      },
+      {
+        name: "TailwindCSS",
+        color: "green-600",
+      },
+    ],
+    img: "https://static2.larioja.com/www/multimedia/202203/13/media/VenderCoche_DaniMaiz.gif",
+    link: "https://",
+    code: "",
+  },
+];
