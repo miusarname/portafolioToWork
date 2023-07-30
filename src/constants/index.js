c24bdcf78725b09297d3f0f63cfeb6d197da7e63
miusarname <oscarm.alvarezg@gmail.com>
2023-07-29 20:10:07 -0500

    feat: :sparkles: Nuevo Proyecto

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,20 +1,20 @@
 export const projects = [
   {
-    title: "On work..",
-    desc: "On work..",
+    title: "The Ultimate Calculator...",
+    desc: "Is an advanced calculator app developed using Flutter, designed to provide a wide range of mathematical functions with a user-friendly interface. The app supports operations like addition, subtraction, multiplication, division, exponentiation, and more. Users can access their calculation history, personalize the theme, and enjoy an intuitive experience. The project aims to offer a powerful and versatile calculator for both Android and iOS devices, enabling users to perform complex mathematical computations with ease.",
     tags: [
       {
-        name: "NextTs",
+        name: "Flutter",
         color: "red-600",
       },
       {
-        name: "TailwindCSS",
+        name: "math_expressions",
         color: "green-600",
       },
     ],
-    img: "https://static2.larioja.com/www/multimedia/202203/13/media/VenderCoche_DaniMaiz.gif",
-    link: "https://",
-    code: "",
+    img: "https://raw.githubusercontent.com/miusarname/Calculadora-FInal/main/docs/capture.png",
+    link: "https://github.com/miusarname/Calculadora-FInal/tree/main",
+    code: "https://github.com/miusarname/Calculadora-FInal/tree/main",
   },
   {
     title: "On work..",
